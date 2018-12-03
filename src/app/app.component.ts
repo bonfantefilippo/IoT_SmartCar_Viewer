13f4b67b10ee01964455c045ad7365b8ebf970a5
Filippo Bonfante <bonfantefilippo@gmail.com>
2018-12-03 23:48:53 +0100

    configurazione per acquisizione dati via websocket da hivemqt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MqttService, IMqttMessage } from 'ngx-mqtt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'SmarcCartViewer';
+
+export class AppComponent implements OnDestroy {
+
+  private subscription: Subscription;
+  public message: string;
+
+  /**
+   *
+   */
+  constructor(private _mqttService: MqttService) {
+    this.subscription = this._mqttService.observe('smartcar/v1/+/+').subscribe((message: IMqttMessage) => {
+      this.message = message.payload.toString();
+    });
+
+  }
+
+  public unsafePublish(topic: string, message: string): void {
+    this._mqttService.unsafePublish(topic, message, {qos: 1, retain: true});
+  }
+
+  public ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MqttModule, IMqttServiceOptions} from 'ngx-mqtt';
 import { AppComponent } from './app.component';
+
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: 'broker.hivemq.com',
+  port: 8000,
+  path: '/mqtt',
+  protocol: 'ws'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
   ],
   providers: [],
   bootstrap: [AppComponent]
