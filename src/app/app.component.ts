8a5c7887afeaadd606cfb9e09a5fb5718c27dbe7
Filippo Bonfante <bonfantefilippo@gmail.com>
2018-12-06 01:15:43 +0100

    implementata ricerca per targa; modificato layout; integrato angular material;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,45 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { MqttService, IMqttMessage } from 'ngx-mqtt';
+import { Component, OnDestroy, Input } from "@angular/core";
+import { Subscription } from "rxjs";
+import { MqttService, IMqttMessage } from "ngx-mqtt";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-
 export class AppComponent implements OnDestroy {
+  @Input()
+  public carPlate: string;
 
   private subscription: Subscription;
   public message: string;
+  public topic: string;
 
   /**
    *
    */
-  constructor(private _mqttService: MqttService) {
-    this.subscription = this._mqttService.observe('smartcar/v1/+/+').subscribe((message: IMqttMessage) => {
-      this.message = message.payload.toString();
-    });
-
-  }
+  constructor(private _mqttService: MqttService) {}
 
   public unsafePublish(topic: string, message: string): void {
-    this._mqttService.unsafePublish(topic, message, {qos: 1, retain: true});
+    this._mqttService.unsafePublish(topic, message, { qos: 1, retain: true });
   }
 
   public ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
-
+  searchCar(form: NgForm) {
+    if (form.valid) {
+      this.carPlate = form.value;
+      this.topic = 'smartcar/v1/' + this.carPlate + '/+';
+      this.subscription = this._mqttService
+        .observe("smartcar/v1/+/+")
+        .subscribe((message: IMqttMessage) => {
+          console.log(message.topic);
+          this.message = message.payload.toString();
+          console.table(this.message);
+        });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
+import {MatIconModule} from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MqttModule, IMqttServiceOptions} from 'ngx-mqtt';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {  FormsModule } from '@angular/forms';
 
 export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
   hostname: 'broker.hivemq.com',
@@ -15,8 +20,12 @@ export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
+    FormsModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS),
+    BrowserAnimationsModule, MatFormFieldModule, MatInputModule, MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
